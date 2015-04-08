e8c09898dde6f2e49b218b91833919d596a13081
Tomiblanchard <tomiblanchard@gmail.com>
2015-04-08 11:48:05 +0100

    signing up

--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
 require 'tag'
+require 'user'
 
 env = ENV['RACK_ENV'] || 'development'
 
@@ -11,6 +12,7 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+
 
   get '/' do
     @links = Link.all
@@ -31,6 +33,16 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(:text => params[:text])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    User.create(email: params[:email],
+                password: params[:password])
+    redirect to('/')
   end
 
   run! if app_file == $PROGRAM_NAME
